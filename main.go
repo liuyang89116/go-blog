358c3291bee69ce141f1124be1935788f6b50074
liuyang89116 <liuyang89116@qq.com>
2022-03-19 15:41:23 -0700

    Setup project structure

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"go-blog/config"
+	"go-blog/models"
 	"log"
 	"net/http"
-	"os"
 	"text/template"
+	"time"
 )
 
 type IndexData struct {
@@ -12,28 +14,72 @@ type IndexData struct {
 	Desc  string `json:"desc"`
 }
 
+func IsOdd(num int) bool {
+	return num%2 == 0
+}
+
+func GetNextName(strs []string, index int) string {
+	return strs[index+1]
+}
+
+func Date(layout string) string {
+	return time.Now().Format(layout)
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	data := IndexData{
-		Title: "Learn Go",
-		Desc:  "Keep learning, keep improving",
-	}
 	t := template.New("index.html")
 	// 拿到当前路径
-	path, _ := os.Getwd()
+	path := config.Cfg.System.CurrentDir
 
-	home := path + "template/home.html"
-	header := path + "template/layout/header.html"
-	footer := path + "template/layout/footer.html"
-	pagination := path + "template/layout/pagination.html"
-	personal := path + "template/layout/personal.html"
-	postList := path + "template/layout/post-list.html"
+	home := path + "/template/home.html"
+	header := path + "/template/layout/header.html"
+	footer := path + "/template/layout/footer.html"
+	pagination := path + "/template/layout/pagination.html"
+	personal := path + "/template/layout/personal.html"
+	postList := path + "/template/layout/post-list.html"
+
+	t.Funcs(template.FuncMap{"isOdd": IsOdd,
+		"getNextName": GetNextName, "date": Date})
+
 	// 因为首页有多个模板嵌套，解析的时候需要把它们都解析出来
-	t, _ = t.ParseFiles(path+"/template/index.html",
+	t, err := t.ParseFiles(path+"/template/index.html",
 		home, header, footer, pagination, personal, postList)
+	if err != nil {
+		log.Println("Parse template error:", err)
+	}
+
 	// 必须定义页面上所有的数据
+	categorys := []models.Category{
+		{
+			Cid:  1,
+			Name: "go",
+		},
+	}
+	posts := []models.PostMore{
+		{
+			Pid:          1,
+			Title:        "Bob's blog",
+			Content:      "Demo",
+			UserName:     "Bob",
+			ViewCount:    123,
+			CreateAt:     "2022-03-19",
+			CategoryId:   1,
+			CategoryName: "go",
+			Type:         0,
+		},
+	}
+	homeResponse := &models.HomeResponse{
+		Viewer:    config.Cfg.Viewer,
+		Categorys: categorys,
+		Posts:     posts,
+		Total:     1,
+		Page:      1,
+		Pages:     []int{1},
+		PageEnd:   true,
+	}
 
 	// execute data
-	t.Execute(w, data)
+	t.Execute(w, homeResponse)
 }
 
 func main() {
@@ -45,6 +91,9 @@ func main() {
 
 	// handle request
 	http.HandleFunc("/", index)
+	// 静态资源配置
+	http.Handle("/resource/",
+		http.StripPrefix("/resource/", http.FileServer(http.Dir("public/resource/"))))
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
---
+++ models/Category.go
@@ -0,0 +1,9 @@
+package models
+
+// Category fetch from db
+type Category struct {
+	Cid      int
+	Name     string
+	CreateAt string
+	UpdateAt string
+}
